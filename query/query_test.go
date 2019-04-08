48f8b59f9013aa2a848d8e07720e6d6106534d47
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-04-08 18:57:09 -0400

    Added time

--- query/query_test.go
+++ query/query_test.go
@@ -5,19 +5,20 @@ import (
 	"io/ioutil"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestExtractJson(t *testing.T) {
 
 	dat, err := ioutil.ReadFile("../fixtures/sample.json")
 	if err != nil {
-		t.Fatalf("%v\n",err)
+		t.Fatalf("%v\n", err)
 	}
 
-	p,_ := ExtractJson(dat)
+	p, _ := ExtractJson(dat)
 
-	for _,v := range p.Data.Result {
-		if strings.Contains(v.Metric.Hostname,"slice-") != true {
+	for _, v := range p.Data.Result {
+		if strings.Contains(v.Metric.Hostname, "slice-") != true {
 			t.Fatalf("Didn't parse")
 		}
 
@@ -28,4 +29,8 @@ func TestExtractJson(t *testing.T) {
 	fmt.Printf("%v\n", len(p.Data.Result))
 	fmt.Printf("%v\n", p.Data.Result[0].Metric.Job)
 	fmt.Printf("%v\n", p.Data.Result[0].Values[0][1])
+	fmt.Printf("%v\n", p.Data.Result[0].Values[0][0])
+	fmt.Printf("%d\n", int64(p.Data.Result[0].Values[0][0].(float64)*1000))
+	mtime := int64(p.Data.Result[0].Values[0][0].(float64)*1000) * 1000000
+	fmt.Printf("%s", time.Unix(0, mtime))
 }
