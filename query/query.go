19f5f7b40646d99f9c63024fb30bef41d50bbc38
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-04-08 18:57:09 -0400

    Added time

--- query/query.go
+++ query/query.go
@@ -2,6 +2,11 @@ package query
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
 
 type Metric struct {
@@ -12,6 +17,7 @@ type Metric struct {
 	It       string `json:"int"`
 	IP       string `json:"ip"`
 	Job      string `json:"job"`
+	Mode     string `json:"mode"`
 }
 
 type R struct {
@@ -37,16 +43,83 @@ type Member struct {
 	Stuff          string
 }
 
-
 func ExtractJson(input []byte) (prom, error) {
 	p := prom{}
 	err := json.Unmarshal(input, &p)
 	if err != nil {
-		return p,err
+		return p, err
 	}
 
-	return p,nil
+	return p, nil
 }
 
+func PullData(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	return body, err
 
+}
 
+func GetTime(i interface{}) time.Time {
+
+	mtime := int64(i.(float64)*1000) * 1000000
+	return time.Unix(0, mtime)
+
+}
+
+func WriteCSV(file string, p prom) error {
+
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Find max
+	max := 0
+	for _, v := range p.Data.Result {
+		if max < len(v.Values) {
+			max = len(v.Values)
+		}
+	}
+
+	for idx := 0; idx < max; idx++ {
+		if idx == 0 {
+			for i, v := range p.Data.Result {
+				if i == 0 {
+					f.WriteString("date")
+				}
+				f.WriteString(fmt.Sprintf(",%s", v.Metric.Hostname))
+			}
+			f.WriteString("\n")
+		}
+
+		for i, v := range p.Data.Result {
+
+			if i == 0 {
+				if len(v.Values) > idx {
+					dateTime := fmt.Sprintf("%s", GetTime(v.Values[idx][0]))
+					f.WriteString(dateTime)
+				}
+			}
+
+			if len(v.Values) > idx {
+				if len(v.Values[idx]) >= 2 {
+					f.WriteString(fmt.Sprintf(",%v", v.Values[idx][1]))
+				}
+
+			} else {
+				f.WriteString(",")
+			}
+
+		}
+		f.WriteString("\n")
+	}
+
+	return nil
+
+}
--- query/query_test.go
+++ query/query_test.go
@@ -5,19 +5,20 @@ import (
 	"io/ioutil"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestExtractJson(t *testing.T) {
 
 	dat, err := ioutil.ReadFile("../fixtures/sample.json")
 	if err != nil {
-		t.Fatalf("%v\n",err)
+		t.Fatalf("%v\n", err)
 	}
 
-	p,_ := ExtractJson(dat)
+	p, _ := ExtractJson(dat)
 
-	for _,v := range p.Data.Result {
-		if strings.Contains(v.Metric.Hostname,"slice-") != true {
+	for _, v := range p.Data.Result {
+		if strings.Contains(v.Metric.Hostname, "slice-") != true {
 			t.Fatalf("Didn't parse")
 		}
 
@@ -27,5 +28,64 @@ func TestExtractJson(t *testing.T) {
 	}
 	fmt.Printf("%v\n", len(p.Data.Result))
 	fmt.Printf("%v\n", p.Data.Result[0].Metric.Job)
+	//fmt.Printf("%v\n", p.Data.Result[0].Metric.Hostname)
+	//fmt.Printf("%v\n", p.Data.Result[1].Metric.Hostname)
 	fmt.Printf("%v\n", p.Data.Result[0].Values[0][1])
+	fmt.Printf("%v\n", p.Data.Result[0].Values[0][0])
+	fmt.Printf("%s\n", GetTime(p.Data.Result[0].Values[1][0]))
+	fmt.Printf("len: %d\n", len(p.Data.Result[0].Values))
+}
+
+func TestWriteCSV(t *testing.T) {
+
+	dat, err := ioutil.ReadFile("../fixtures/5d")
+	if err != nil {
+		t.Fatalf("%v\n", err)
+	}
+
+	p, _ := ExtractJson(dat)
+
+	WriteCSV("junk.csv", p)
+
+}
+
+func sTestPullData(t *testing.T) {
+	url := `http://vovprm-po-1p.sys.comcast.net:9090/api/v1/query?query=cos_device_network_receive_drop{device=%22slicestor%22,%20int=%22bond0%22}[1h]`
+	url = `http://vovprm-po-1p.sys.comcast.net:9090/api/v1/query?query=cos_device_cpu{device=%22slicestor%22}[1h]`
+	data, _ := PullData(url)
+	p, _ := ExtractJson(data)
+	fmt.Printf("%v\n", p.Data.Result[0].Metric.Job)
+
+	for _, v := range p.Data.Result {
+		if strings.Contains(v.Metric.Hostname, "slice-") != true {
+			t.Fatalf("Didn't parse")
+		}
+
+		mtime := int64(v.Values[0][0].(float64)*1000) * 1000000
+		value := v.Values[0][1]
+		host := v.Metric.Hostname
+		fmt.Printf("%s: %s, %v\n", host, time.Unix(0, mtime), value)
+
+	}
+
+}
+
+func sTest2(t *testing.T) {
+	url := `http://vovprm-po-1p.sys.comcast.net:9090/api/v1/query_range?query=cos_device_network_receive_drop{device=%22slicestor%22,%20int=%22bond0%22}&start=2019-04-01T20:10:30.781Z&end=2019-04-06T20:11:00.781Z&step=20m`
+
+	//url = `http://vovprm-po-1p.sys.comcast.net:9090/api/v1/query?query=cos_device_cpu{device=%22slicestor%22}[1h]`
+	data, _ := PullData(url)
+	p, _ := ExtractJson(data)
+	fmt.Printf("%v\n", p.Data.Result[0].Metric.Job)
+
+	for _, v := range p.Data.Result {
+		if strings.Contains(v.Metric.Hostname, "slice-") != true {
+			t.Fatalf("Didn't parse")
+		}
+
+		if v.Metric.Hostname == "slice-ch2-a10p.sys.comcast.net" {
+			fmt.Printf("Value: %v\n", v.Values[0][1])
+		}
+	}
+
 }
